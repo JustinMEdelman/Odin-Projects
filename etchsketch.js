f1f1a6ae46a31aeeec1f21a3bf09dffc95b98421
Justin Edelman <justinmedelman@gmail.com>
2022-01-17 21:22:45 -0500

    js update

--- etchsketch.js
+++ etchsketch.js
@@ -12,10 +12,8 @@ function sizeSubmit(){
     let inputAmount = Number(document.getElementById('inputAmount').value);
     if (!inputAmount){
         gridCreate(16);
-        document.getElementById('warningMessage').style.display = 'none';
     } else if(inputAmount < 101){
         gridCreate(inputAmount);
-        document.getElementById('warningMessage').style.display = 'none';
     }
     else {
         inputCheck();
@@ -32,8 +30,7 @@ function colorFun(){
         gridDivs[i].addEventListener('mouseover', function(e){
         let targetColor = e.target.style.backgroundColor;
         if (!targetColor){
-            let bgColor = randomColor();
-            e.target.style.backgroundColor = bgColor;
+            e.target.style.backgroundColor = randomColor();
             }
      }
     )}
@@ -43,7 +40,6 @@ function colorFun(){
 function gridCreate(inputAmount){
     let totalDivs = inputAmount * inputAmount
     let divWH = 600 / inputAmount;
-    console.log(divWH);
     gridContainer.style.gridTemplateColumns = `repeat(${inputAmount}, ${divWH}px)`;
     gridContainer.style.gridTemplateRows = `repeat(${inputAmount}, ${divWH}px)`;
     for (let i = 1; i < (totalDivs +1); i++){
